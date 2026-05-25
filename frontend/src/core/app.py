76cde2094289dd403a3225986b7fe225cce0cbed
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-25 10:20:29 +0530

    minor: merge contexts.py into app.py

--- frontend/src/core/app.py
+++ frontend/src/core/app.py
@@ -1,6 +1,7 @@
 import flet as ft
 
-from core.contexts import ContextWrapper
+from constants.routes import ROUTE_ROOT
+from contexts.theme import ThemeContext, ThemeContextValue
 from core.events import subscribe_events
 from core.router import Router
 from state.app_state import AppState
@@ -14,7 +15,35 @@ def App() -> ft.Control:
 
 	subscribe_events(app_state)
 
-	return ContextWrapper(
-		app_state=app_state,
-		callback=Router
+	toggle_mode = ft.use_callback(
+		lambda: app_state.toggle_theme(),
 	)
+
+	theme_context = ft.use_memo(
+		lambda: ThemeContextValue(
+			mode=app_state.theme_mode,
+			seed_color=app_state.theme_color,
+			toggle_mode=toggle_mode,
+		),
+		dependencies=[
+			app_state.theme_mode,
+			app_state.theme_color,
+			toggle_mode,
+		],
+	)
+
+	theme_wrapped = ThemeContext(
+		value=theme_context,
+		callback=lambda: ft.View(
+			route=ROUTE_ROOT,
+			appbar=None,
+			controls=[
+				ft.SafeArea(
+					expand=True,
+					content=Router(),
+				),
+			],
+		),
+	)
+
+	return theme_wrapped
